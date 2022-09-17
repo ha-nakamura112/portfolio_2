6c93f50e111105526c0b95f3127bf7bd70c07b7d
ha-nakamura112 <nh.3213.b.b@gmail.com>
2022-09-18 00:52:48 +0900

    mid project profile java done

--- script/button.js
+++ script/button.js
@@ -2,15 +2,25 @@ var img;
 function sampleimg1(){
 img = document.getElementById("image_file");
 img.src = "./img/project1.png";
+
+a = document.getElementById("image_tag");
+a.href = "https://incomparable-pavlova-474e4f.netlify.app";
 }
 
 function sampleimg2(){
 img = document.getElementById("image_file");
 img.src = "./img/project2(2).png";
+
+a = document.getElementById("image_tag");
+a.href = "https://earnest-marzipan-fee446.netlify.app";
 }
 
 function sampleimg3(){
 img = document.getElementById("image_file");
 img.src ="./img/project3(1).png";
+
+a = document.getElementById("image_tag");
+a.href = "https://earnest-griffin-ed5b78.netlify.app";
 }
+
 
