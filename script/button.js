6c169aa2f65f81442188b80b101ea4dfbe2fd16a
ha-nakamura112 <nh.3213.b.b@gmail.com>
2022-09-17 23:44:00 +0900

    mid project profile java done

--- script/button.js
+++ script/button.js
@@ -1,24 +1,3 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-
-const alert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    alert('Thank you!', 'success')
-  })
-}
-
 var img;
 function sampleimg1(){
 img = document.getElementById("image_file");
@@ -34,19 +13,4 @@ function sampleimg3(){
 img = document.getElementById("image_file");
 img.src ="./img/project3(1).png";
 }
-
-$(function(){
-  $(window).scroll(function (){
-      $('.fadein').each(function(){
-          var position = $(this).offset().top;
-          var scroll = $(window).scrollTop();
-          var windowHeight = $(window).height();
-          if (scroll > position - windowHeight + 300){
-            $(this).addClass('active');
-          }else{
-            $(this).removeClass('active');
-          }
-      });
-  });
-});
 
